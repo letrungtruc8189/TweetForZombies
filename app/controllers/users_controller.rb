ebaad14f0e91ba7e414fb3c78db3ede48f48a2ee
truc_le <letrungtruc8189@gmail.com>
2017-05-26 22:54:42 +0700

    show and edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,35 @@
 class UsersController < ApplicationController
   def new
+    @user = User.all
+
   end
 
   def index
     @user = User.all
+
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path(@user)
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role)
   end
 end
