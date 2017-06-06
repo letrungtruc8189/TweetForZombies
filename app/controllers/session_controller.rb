83b3714fc0de779bec4d6a01b91ba3d1c975f23c
TRUC LE <trucle@tinypulse.com>
2017-06-06 16:53:23 +0700

    done commit

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,10 +6,16 @@ class SessionController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      redirect_to rooms_path
+      log_in(@user)
+      redirect_to users_path
     else
       render 'new'
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to signin_path
   end
 
 end
