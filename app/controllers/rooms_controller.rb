ae1f04a4098034572b13d79fd4dbeb61a9aba9dd
TRUC LE <trucle@tinypulse.com>
2017-06-15 14:34:13 +0700

    change room controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,49 +1,41 @@
 
   class RoomsController < ActionController::Base
-    # GET /rooms
-    # GET /rooms.json
+
     def index
       @rooms = Room.all
     end
 
-    # GET /rooms/1
-    # GET /rooms/1.json
     def show
       @room = Room.find(params[:id])
     end
 
-    # GET /rooms/new
     def new
       @room = Room.new
     end
 
-    # GET /rooms/1/edit
     def edit
       @room = Room.find(params[:id])
     end
 
-    # POST /rooms
-    # POST /rooms.json
     def create
       @room = Room.new(room_params)
 
-      respond_to do |format|
-        if @room.save
-          format.html { redirect_to rooms_path, notice: 'Room was successfully created.' }
-          format.json { render :show, status: :created, location: @room }
-        else
-          format.html { render :new }
-          format.json { render json: @room.errors, status: :unprocessable_entity }
-        end
+      if @room.save
+        flash[:success] = "Room is created successfully"
+        render 'show'
+      else
+        flash[:danger] = @room.errors
+        render 'new'
+      end
       end
     end
 
-    # PATCH/PUT /rooms/1
-    # PATCH/PUT /rooms/1.json
     def update
       @room = Room.find(params[:id])
       respond_to do |format|
         if @room.update(room_params)
+          flash[:success] = "Room is created successfully"
+          render 'show'
           format.html { redirect_to @room, notice: 'Room was successfully updated.' }
           format.json { render :show, status: :ok, location: @room }
         else
@@ -53,8 +45,6 @@
       end
     end
 
-    # DELETE /rooms/1
-    # DELETE /rooms/1.json
     def destroy
       @room = Room.find(params[:id]).destroy
       redirect_to rooms_path
